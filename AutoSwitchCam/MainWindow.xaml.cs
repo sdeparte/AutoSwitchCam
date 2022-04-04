dfb86714a24fe89cf40a606f9d1d219447debc33
sdeparte <sylvain.departe@gmail.com>
2022-04-04 13:14:38 +0200

    Add edit zone feature

--- AutoSwitchCam/MainWindow.xaml.cs
+++ AutoSwitchCam/MainWindow.xaml.cs
@@ -3,11 +3,10 @@ using AForge.Video.DirectShow;
 using AutoSwitchCam.Helper;
 using AutoSwitchCam.Model;
 using AutoSwitchCam.Services;
-using Microsoft.Kinect;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows;
@@ -98,7 +97,7 @@ namespace AutoSwitchCam
             }
 
             CameraStreamSelectBox.SelectedIndex = 0;
-            CameraZoneSelectBox.SelectedIndex = 0;
+            AddCameraZoneSelectBox.SelectedIndex = 0;
         }
 
         private void InitHelpersSelectors()
@@ -192,7 +191,7 @@ namespace AutoSwitchCam
                 captureDevice.NewFrame -= PreviewVideoCapture_NewFrame;
             }
 
-            VideoCaptureDevice videoCaptureDevice = _videoCaptureDevices[CameraZoneSelectBox.SelectedIndex];
+            VideoCaptureDevice videoCaptureDevice = _videoCaptureDevices[AddCameraZoneSelectBox.SelectedIndex];
 
             videoCaptureDevice.NewFrame += PreviewVideoCapture_NewFrame;
         }
@@ -213,7 +212,8 @@ namespace AutoSwitchCam
                     bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
                     bitmapImage.EndInit();
 
-                    Image.Source = bitmapImage;
+                    AddPreviewImage.Source = bitmapImage;
+                    EditPreviewImage.Source = bitmapImage;
                 }));
             }
         }
@@ -230,53 +230,125 @@ namespace AutoSwitchCam
             switch (PointsSelectBox.SelectedIndex)
             {
                 case 0:
-                    X1.Text = head.X.ToString();
-                    Z1.Text = head.Z.ToString();
+                    AddX1TextBox.Text = head.X.ToString();
+                    AddZ1TextBox.Text = head.Z.ToString();
                     break;
 
                 case 1:
-                    X2.Text = head.X.ToString();
-                    Z2.Text = head.Z.ToString();
+                    AddX2TextBox.Text = head.X.ToString();
+                    AddZ2TextBox.Text = head.Z.ToString();
                     break;
 
                 case 2:
-                    X3.Text = head.X.ToString();
-                    Z3.Text = head.Z.ToString();
+                    AddX3TextBox.Text = head.X.ToString();
+                    AddZ3TextBox.Text = head.Z.ToString();
                     break;
 
                 case 3:
-                    X4.Text = head.X.ToString();
-                    Z4.Text = head.Z.ToString();
+                    AddX4TextBox.Text = head.X.ToString();
+                    AddZ4TextBox.Text = head.Z.ToString();
                     break;
             }
         }
-        
+
         private void AddZone_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(X1.Text) || string.IsNullOrEmpty(Z1.Text) ||
-                string.IsNullOrEmpty(X2.Text) || string.IsNullOrEmpty(Z2.Text) ||
-                string.IsNullOrEmpty(X3.Text) || string.IsNullOrEmpty(Z3.Text) ||
-                string.IsNullOrEmpty(X4.Text) || string.IsNullOrEmpty(Z4.Text) ||
-                ColorHelper.IsColorString.IsMatch(ColorTextBox.Text))
+            if (string.IsNullOrEmpty(AddX1TextBox.Text) || string.IsNullOrEmpty(AddZ1TextBox.Text) ||
+                string.IsNullOrEmpty(AddX2TextBox.Text) || string.IsNullOrEmpty(AddZ2TextBox.Text) ||
+                string.IsNullOrEmpty(AddX3TextBox.Text) || string.IsNullOrEmpty(AddZ3TextBox.Text) ||
+                string.IsNullOrEmpty(AddX4TextBox.Text) || string.IsNullOrEmpty(AddZ4TextBox.Text) ||
+                !ColorHelper.IsColorString.IsMatch(AddColorTextBox.Text))
             {
                 return;
             }
 
             _listZones.Add(new Zone()
             {
-                Color = ColorTextBox.Text,
-                CameraName = ((Camera)CameraZoneSelectBox.SelectedItem).FilterInfo.Name,
-                CameraIndex = CameraZoneSelectBox.SelectedIndex,
-                X1 = Convert.ToDouble(X1.Text),
-                Z1 = Convert.ToDouble(Z1.Text),
-                X2 = Convert.ToDouble(X2.Text),
-                Z2 = Convert.ToDouble(Z2.Text),
-                X3 = Convert.ToDouble(X3.Text),
-                Z3 = Convert.ToDouble(Z3.Text),
-                X4 = Convert.ToDouble(X4.Text),
-                Z4 = Convert.ToDouble(Z4.Text),
+                Color = AddColorTextBox.Text,
+                CameraName = ((Camera)AddCameraZoneSelectBox.SelectedItem).FilterInfo.Name,
+                CameraIndex = AddCameraZoneSelectBox.SelectedIndex,
+                X1 = double.Parse(AddX1TextBox.Text, CultureInfo.InvariantCulture),
+                Z1 = double.Parse(AddZ1TextBox.Text, CultureInfo.InvariantCulture),
+                X2 = double.Parse(AddX2TextBox.Text, CultureInfo.InvariantCulture),
+                Z2 = double.Parse(AddZ2TextBox.Text, CultureInfo.InvariantCulture),
+                X3 = double.Parse(AddX3TextBox.Text, CultureInfo.InvariantCulture),
+                Z3 = double.Parse(AddZ3TextBox.Text, CultureInfo.InvariantCulture),
+                X4 = double.Parse(AddX4TextBox.Text, CultureInfo.InvariantCulture),
+                Z4 = double.Parse(AddZ4TextBox.Text, CultureInfo.InvariantCulture),
             });
+
             _configReader.updateConfigFiles(this);
+        }
+
+        private void AddColorTextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ColorHelper.IsColorString.IsMatch(AddColorTextBox.Text))
+            {
+                System.Drawing.Color color = ColorHelper.ToColor(AddColorTextBox.Text);
+                AddPreviewColorRectangle.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(color.R, color.G, color.B));
+            }
+            else
+            {
+                AddPreviewColorRectangle.Fill = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void EditZone_Click(object sender, RoutedEventArgs e)
+        {
+            if (ZonesDataGrid.SelectedIndex >= 0)
+            {
+                if (string.IsNullOrEmpty(EditX1TextBox.Text) || string.IsNullOrEmpty(EditZ1TextBox.Text) ||
+                    string.IsNullOrEmpty(EditX2TextBox.Text) || string.IsNullOrEmpty(EditZ2TextBox.Text) ||
+                    string.IsNullOrEmpty(EditX3TextBox.Text) || string.IsNullOrEmpty(EditZ3TextBox.Text) ||
+                    string.IsNullOrEmpty(EditX4TextBox.Text) || string.IsNullOrEmpty(EditZ4TextBox.Text) ||
+                    !ColorHelper.IsColorString.IsMatch(EditColorTextBox.Text))
+                {
+                    return;
+                }
+
+                Zone zone = _listZones[ZonesDataGrid.SelectedIndex];
+
+                zone.Color = EditColorTextBox.Text;
+                zone.CameraName = ((Camera)EditCameraZoneSelectBox.SelectedItem).FilterInfo.Name;
+                zone.CameraIndex = EditCameraZoneSelectBox.SelectedIndex;
+                zone.X1 = double.Parse(EditX1TextBox.Text, CultureInfo.InvariantCulture);
+                zone.Z1 = double.Parse(EditZ1TextBox.Text, CultureInfo.InvariantCulture);
+                zone.X2 = double.Parse(EditX2TextBox.Text, CultureInfo.InvariantCulture);
+                zone.Z2 = double.Parse(EditZ2TextBox.Text, CultureInfo.InvariantCulture);
+                zone.X3 = double.Parse(EditX3TextBox.Text, CultureInfo.InvariantCulture);
+                zone.Z3 = double.Parse(EditZ3TextBox.Text, CultureInfo.InvariantCulture);
+                zone.X4 = double.Parse(EditX4TextBox.Text, CultureInfo.InvariantCulture);
+                zone.Z4 = double.Parse(EditZ4TextBox.Text, CultureInfo.InvariantCulture);
+
+                _configReader.updateConfigFiles(this);
+            }
+
+            ClosePopInButton.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+        }
+
+        private void EditColorTextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ColorHelper.IsColorString.IsMatch(EditColorTextBox.Text))
+            {
+                System.Drawing.Color color = ColorHelper.ToColor(EditColorTextBox.Text);
+                EditPreviewColorRectangle.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(color.R, color.G, color.B));
+            }
+            else
+            {
+                EditPreviewColorRectangle.Fill = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void DeleteZone_Click(object sender, RoutedEventArgs e)
+        {
+            if (ZonesDataGrid.SelectedIndex >= 0)
+            {
+                _listZones.RemoveAt(ZonesDataGrid.SelectedIndex);
+
+                _configReader.updateConfigFiles(this);
+            }
+
+            ClosePopInButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
         }
 
         private void ColorValidationTextBox(object sender, TextCompositionEventArgs e)
@@ -310,17 +382,33 @@ namespace AutoSwitchCam
             }
         }
 
-        private void ColorTextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        private void ZonesDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (ColorHelper.IsColorString.IsMatch(ColorTextBox.Text))
+            ListView listView = (ListView)sender;
+            Zone zone = (Zone)listView.SelectedItem;
+
+            if (zone != null)
             {
-                System.Drawing.Color color = ColorHelper.ToColor(ColorTextBox.Text);
-                PrviewColorRectangle.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(color.R, color.G, color.B));
+                EditGrid.Visibility = Visibility.Visible;
+
+                EditCameraZoneSelectBox.SelectedIndex = zone.CameraIndex;
+                EditColorTextBox.Text = zone.Color;
+                EditX1TextBox.Text = zone.X1.ToString(null, CultureInfo.InvariantCulture);
+                EditZ1TextBox.Text = zone.Z1.ToString(null, CultureInfo.InvariantCulture);
+                EditX2TextBox.Text = zone.X2.ToString(null, CultureInfo.InvariantCulture);
+                EditZ2TextBox.Text = zone.Z2.ToString(null, CultureInfo.InvariantCulture);
+                EditX3TextBox.Text = zone.X3.ToString(null, CultureInfo.InvariantCulture);
+                EditZ3TextBox.Text = zone.Z3.ToString(null, CultureInfo.InvariantCulture);
+                EditX4TextBox.Text = zone.X4.ToString(null, CultureInfo.InvariantCulture);
+                EditZ4TextBox.Text = zone.Z4.ToString(null, CultureInfo.InvariantCulture);
             }
-            else
-            {
-                PrviewColorRectangle.Fill = System.Windows.Media.Brushes.Black;
-            }
+        }
+
+        private void ClosePopInButton_Click(object sender, RoutedEventArgs e)
+        {
+            ZonesDataGrid.SelectedIndex = -1;
+            ZonesDataGrid.Items.Refresh();
+            EditGrid.Visibility = Visibility.Hidden;
         }
     }
 }
